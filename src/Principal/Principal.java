79989ab4cbcf154b5c88fdb2fe051859fb3adf5d
Ricky Rhonaldo Ortega Vital <ortegavitalricky@gmail.com>
2023-01-20 10:35:21 -0500

    Funcionando Ejercicio 2 Taller 3

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -2,7 +2,7 @@ package Principal;
 
 public class Principal {
     public static void main(String[] args) {
-        Taller3.Ejercicio1 ejercicio1 = new Taller3.Ejercicio1();
-        ejercicio1.ejecutar();
+        Taller3.Ejercicio2 ejercicio2 = new Taller3.Ejercicio2();
+        ejercicio2.ejecutar();
     }
 }
--- src/Taller3/Ejercicio2.java
+++ src/Taller3/Ejercicio2.java
@@ -1,2 +1,26 @@
-package Taller3;public class Ejercicio2 {
+/*Realizar la siguiente figura por medio del ciclo Mientras - Hacer*/
+
+package Taller3;
+
+import Principal.Ejercicio;
+
+public class Ejercicio2 extends Ejercicio {
+
+    private void mostrarFigura(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j < n - i - 1) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    @Override
+    public void ejecutar() {
+        mostrarFigura(10);
+    }
 }
